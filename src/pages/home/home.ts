4942d0180936144c8ba8b69f18d524675b76f305
ngrostar <win7x64@web.de>
2018-01-28 15:47:09 +0100

    Überschrift zum Testen des Repository geändert

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ParkplatzProvider} from "../../providers/parkplatz/parkplatz";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+    public posts: any;
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, public ParkplatzProvider: ParkplatzProvider) {
+        this.loadpp();
+    }
 
-  }
 
+    loadpp() {
+        this.ParkplatzProvider.getpp()
+            .then(data => {
+                this.posts = data;
+                console.log(this.posts);
+            });
+    }
 }
