6677022f766bc9aa9a1e3c705a8db54094ef4353
Ramona <ramona.plogmann@gmail.com>
2018-02-01 16:28:12 +0100

    marker for stations

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -34,9 +34,12 @@ export class HomePage {
       this.stations = data['result'];
       console.log("STATIONEN");
       console.log(this.stations);
-      let ll = new google.maps.LatLng(50,8);
-      this.map.setCenter(ll);
-      // this.geocode(this.stations[1]);
+      let count = 0;
+      for(let station of this.stations) {
+        if(count<100)
+        this.addSpecificMarker(station);
+        count++;
+      }
     });
 
   }
@@ -48,13 +51,13 @@ export class HomePage {
 
       let mapOptions = {
         center: latLng,
-        zoom: 15,
+        zoom: 14,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
 
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
-      // this.map.setCenter(latLng);
+      this.map.setCenter(latLng);
 
 
     }, (err) => {
@@ -63,40 +66,47 @@ export class HomePage {
 
   }
 
-  addSpecificMarker(post) {
-    console.log("Trying to add marker for " + post.name);
+  addSpecificMarker(station) {
+    console.log("Trying to add marker for " + station.name);
 
-    let latLng = new google.maps.LatLng(post.breite, post.laenge);
-    this.map.setCenter(latLng);
+    for(let eN of station.evaNumbers) {
+      if(eN.isMain) {
+        let coords = eN.geographicCoordinates.coordinates;
+        let latLng = new google.maps.LatLng(coords[1], coords[0]);
 
-    let marker = new google.maps.Marker({
-      map: this.map,
-      color: '#00aaff',
-      animation: google.maps.Animation.DROP,
-      position: latLng
-    });
+        let marker = new google.maps.Marker({
+          map: this.map,
+          color: '#00aaff',
+          animation: google.maps.Animation.DROP,
+          position: latLng
+        });
 
-    let content = "<h6>" + post.name + "</h6><p>Länge: " + post.laenge + "<br>Breite: " + post.breite + "</p>";
+        let content = "<h6>" + station.name + "</h6><p>Länge: " +  coords[1] + "<br>Breite: " + coords[0] + "</p>";
+      }
+    }
 
   }
 
   geocode(station) {
+
     let address = station.mailingAddress.street + " " + station.mailingAddress.zipcode + " " + station.mailingAddress.city;
     console.log('trying to get coords for ' + address);
     this.geocoder.geocode({'address': address}, function (results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
 
-        console.log(results);
-        this.map.setCenter(results[0].geometry.location);
-        // let latLng = new google.maps.LatLng(results[0].geometry.viewport.b.b, results[0].geometry.viewport.f.b);
-        console.log(results[0].geometry.viewport.b.b, results[0].geometry.viewport.f.b, latLng);
-        // this.map.setCenter(latLng);
-        let marker = new google.maps.Marker({
-          map: this.map,
-          position: results[0].geometry.location,
-          // position: latLng,
-          animation: google.maps.Animation.DROP
-        });
+        let ll = new google.maps.LatLng(50,8);
+        this.map.setCenter(ll);
+        // console.log(results);
+        // this.map.setCenter(results[0].geometry.location);
+        // // let latLng = new google.maps.LatLng(results[0].geometry.viewport.b.b, results[0].geometry.viewport.f.b);
+        // console.log(results[0].geometry.viewport.b.b, results[0].geometry.viewport.f.b, latLng);
+        // // this.map.setCenter(latLng);
+        // let marker = new google.maps.Marker({
+        //   map: this.map,
+        //   position: results[0].geometry.location,
+        //   // position: latLng,
+        //   animation: google.maps.Animation.DROP
+        // });
       } else {
         console.log('Geocode for ' + station.name + ' was not successful for the following reason: ' + status);
       }
