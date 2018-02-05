005399f9e997c9c9fb1c0771dd279b1640dd56a6
ngrostar <win7x64@web.de>
2018-02-05 13:15:34 +0100

    Änderungen ts Parkplatz

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {ParkplatzProvider} from "../../providers/parkplatz/parkplatz";
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public Parkplatz:ParkplatzProvider) {
+    this.loadParkplatz('spaces');
   }
+  pp:any;
+    loadParkplatz(param) {
+        this.Parkplatz.load(param).then(data => {
+
+            this.pp = data['result'];
+            console.log("Parkplätze");
+            console.log(this.pp);
+        });
+    }
 
 }
---
+++ src/providers/parkplatz/parkplatz.ts
@@ -0,0 +1,30 @@
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+
+/*
+  Generated class for the ParkplatzProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ParkplatzProvider {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello ParkplatzProvider Provider');
+  }
+    load(param) {
+        return new Promise(resolve => {
+            const headers = new HttpHeaders().set('Authorization', `Bearer 61142190cdeab44f140c58c9fb40d293`);
+            headers.append('Accept','application/json');
+            let url = 'https://api.deutschebahn.com/bahnpark/v1/' + param;
+            this.http.get(url,{headers}).subscribe(data => {
+                resolve(data);
+            }, err => {
+                console.log(err);
+            });
+        });
+    }
+}
