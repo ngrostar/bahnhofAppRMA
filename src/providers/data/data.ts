6489d430ac12a496e3e8bbc2f7777d6209ae2e92
Ramona <ramona.plogmann@gmail.com>
2018-02-08 18:38:23 +0100

    zusammenfuehrung

--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class DataProvider {
   public aktStation: any;
+  public pps:any;
+  public parkingspaces;
 
   constructor() {
     console.log('Hello DataProvider Provider');
